9790bcd8318df0ef33327cba6c56bf6c6771cd47
junaedirahman <im.junaedirahman@gmail.com>
2025-01-23 08:46:12 +0700

    Add files via upload

---
+++ charts-definition.js
@@ -0,0 +1,92 @@
+// Create the charts when the web page loads
+window.addEventListener('load', onload);
+
+function onload(event){
+  chartT = createTemperatureChart();
+  chartH = createHumidityChart();
+}
+
+// Create Temperature Chart
+function createTemperatureChart() {
+  var chart = new Highcharts.Chart({
+    time:{
+      useUTC: false
+      },
+    chart:{ 
+      renderTo:'chart-temperature',
+      type: 'spline' 
+    },
+    series: [
+      {
+        name: ' Sensing Time'
+      }
+    ],
+    title: { 
+      text: undefined
+    },
+    plotOptions: {
+      line: { 
+        animation: false,
+        dataLabels: { 
+          enabled: true 
+        }
+      }
+    },
+    xAxis: {
+      type: 'datetime',
+      dateTimeLabelFormats: { second: '%H:%M:%S' }
+    },
+    yAxis: {
+      title: { 
+        text: 'Temperature (°C)' 
+      }
+    },
+    credits: { 
+      enabled: false 
+    }
+  });
+  return chart;
+}
+
+// Create Humidity Chart
+function createHumidityChart(){
+  var chart = new Highcharts.Chart({
+    time:{
+      useUTC: false
+      },
+    chart:{ 
+      renderTo:'chart-humidity',
+      type: 'spline'  
+    },
+    series: [{
+      name: 'Sensing Time'
+    }],
+    title: { 
+      text: undefined
+    },    
+    plotOptions: {
+      line: { 
+        animation: false,
+        dataLabels: { 
+          enabled: true 
+        }
+      },
+      series: { 
+        color: '#50b8b4' 
+      }
+    },
+    xAxis: {
+      type: 'datetime',
+      dateTimeLabelFormats: { second: '%H:%M:%S' }
+    },
+    yAxis: {
+      title: { 
+        text: 'Humidity (%)' 
+      }
+    },
+    credits: { 
+      enabled: false 
+    }
+  });
+  return chart;
+}
